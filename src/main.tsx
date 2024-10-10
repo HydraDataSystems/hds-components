2165e5d634fb4f68828fb971230126434cfc6664
hleyf <hleyf@hotmail.com>
2024-10-10 15:55:43 +0100

    main quick styling

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,13 @@ import { Button } from "./index";
 const root = ReactDom.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <div>
-    <DatePicker label="My Label" initialDate="2023-11-01" />
+  <div className="flex my-4 mx-20 gap-8">
+    <div className="flex flex-col border-slate-800 border-2 p-4 rounded-md border-opacity-25 hover:border-opacity-75">
+    <DatePicker label="Label" initialDate="2023-11-01" />
+
+    </div>
+    <div className="flex flex-col border-slate-800 border-2 p-4 rounded-md border-opacity-25 hover:border-opacity-75">
     <Button title="My Button" />
+    </div>
   </div>
 );
