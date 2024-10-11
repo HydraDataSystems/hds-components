cb35f7ee23359d05d09b5c3e1b7f40d4e3f8a817
hleyf <hleyf@hotmail.com>
2024-10-11 13:17:36 +0100

    Badge component imp

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { default as Button } from './components/Button';
 export type { ButtonProps } from './components/Button';
 export { default as DatePicker } from './components/DatePicker';
 export type { DatePickerProps } from './components/DatePicker';
+export { default as Badge } from './components/Badge';
+export type { BadgeProps } from './components/Badge';
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import { DatePicker } from "./index";
 import { Button } from "./index";
+import Badge from "./components/Badge";
 
 const root = ReactDom.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <div>
-    <DatePicker label="My Label" initialDate="2023-11-01" />
+  <div className="flex my-4 mx-20 gap-8">
+    <div className="flex flex-col">
+    <DatePicker label="Label" initialDate="2023-11-01" />
+    </div>
+
+    <div className="flex flex-col">
     <Button title="My Button" />
+    </div>
+
+    <div className="flex flex-col gap-2">
+    <Badge type="success" content= "Success" />
+    <Badge type="warning" content= "Warning" />
+    <Badge type="alert" content= "Alert" />
+    </div>
   </div>
 );
