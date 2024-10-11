81855c5a8bc2929e2cc49fc06ced8a539c68222c
hleyf <hleyf@hotmail.com>
2024-10-11 12:23:55 +0100

    main: accordion add and styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,40 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { DatePicker } from "./index";
+import { Accordion, DatePicker } from "./index";
 import { Button } from "./index";
 
 const root = ReactDom.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <div className="flex my-4 mx-20 gap-8">
-    <div className="flex flex-col border-slate-800 border-2 p-4 rounded-md border-opacity-25 hover:border-opacity-75">
+    <div className="flex flex-col">
     <DatePicker label="Label" initialDate="2023-11-01" />
-
     </div>
-    <div className="flex flex-col border-slate-800 border-2 p-4 rounded-md border-opacity-25 hover:border-opacity-75">
+
+    <div className="flex flex-col">
     <Button title="My Button" />
     </div>
+
+    <div className="flex flex-col">
+      <Accordion barContent="Accordion Header">
+        <div className="bg-slate-200 p-4">
+        <table>
+          <tbody>
+
+          <tr>
+            <td>Row 1</td>
+            <td>Row 2</td>
+          </tr>
+          <tr>
+            <td>Row 3</td>
+            <td>Row 4</td>
+          </tr>
+          </tbody>
+        </table>
+        </div>
+      </Accordion>
+    </div>
   </div>
+
+
 );
