2e0a64af741f7a42cac7d42c5824a4498e70f9ba
Francisco <hleyf@hotmail.com>
2024-10-15 12:14:19 +0100

    FormInput file rename

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -26,10 +26,12 @@ const FormInput: React.FC<FormInputProps> = ({
   classNames = "",
   autocomplete,
 }) => {
-
   return (
     <div className={classNames}>
-      <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
         {label}
       </label>
       <div className="mt-1 relative rounded-md shadow-sm">
@@ -37,10 +39,17 @@ const FormInput: React.FC<FormInputProps> = ({
           id={name}
           type={type}
           autoComplete={type === "tel" && autocomplete ? "tel" : undefined}
-          pattern={type === "email" ? /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ : undefined}
+          pattern={
+            type === "email"
+              ? /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+              : undefined
+          }
           {...register(name, { required, validate: { validate } })}
-          className={errors[name] ? "block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6" 
-            : "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}
+          className={
+            errors[name]
+              ? "block w-full rounded-md border-0 py-1.5 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+              : "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          }
         />
       </div>
       {errors[name] && (
