e5638fb6478a5505ef7aa7253d29eecfa5c9e350
Francisco <hleyf@hotmail.com>
2024-10-17 15:35:31 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
   content: ['./src/**/*.{ts,tsx}', './index.html', './src/main.tsx'],
   safelist: [
@@ -7,43 +9,23 @@ export default {
       variants: ['hover', 'focus', 'active'],
     },
     {
+      pattern: /^ring-(gray|green|blue|orange|red)-600\/20/,
+    },
+    {
       pattern: /^(outline|outline-2|outline-transparent)/,
+    },
+    {
+      pattern: /^(bg-red-600|text-white)/,
     },
   ],
   theme: {
     extend: {
-      backgroundColor: {
-        'gray-100': '#f3f4f6',
-        'gray-300': '#d1d5db',
-        'green-100': '#d1fae5',
-        'green-300': '#6ee7b7',
-        'blue-100': '#dbeafe',
-        'blue-300': '#93c5fd',
-        'orange-100': '#ffedd5',
-        'orange-300': '#fdba74',
-        'red-100': '#fee2e2',
-        'red-300': '#fca5a5',
-      },
-      textColor: {
-        'gray-800': '#1f2937',
-        'green-800': '#065f46',
-        'blue-800': '#1e40af',
-        'orange-800': '#9a3412',
-        'red-800': '#991b1b',
-      },
-      borderColor: {
-        'gray-300': '#d1d5db',
-        'green-300': '#6ee7b7',
-        'blue-300': '#93c5fd',
-        'orange-300': '#fdba74',
-        'red-300': '#fca5a5',
-      },
-      ringColor: {
-        'gray-600': '#4b5563',
-        'green-600': '#059669',
-        'blue-600': '#2563eb',
-        'orange-600': '#ea580c',
-        'red-600': '#dc2626',
+      colors: {
+        gray: colors.gray,
+        green: colors.green,
+        blue: colors.blue,
+        orange: colors.orange,
+        red: colors.red,
       },
     },
   },
