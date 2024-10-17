8ec083850a00669b02d18bd48938b9ba649b58e4
Francisco <hleyf@hotmail.com>
2024-10-17 11:35:54 +0100

    components removal

--- src/main.tsx
+++ src/main.tsx
@@ -3,33 +3,12 @@ import "./index.css";
 import DatePicker from "./components/DatePicker";
 import Button from "./components/Button";
 import Accordion from "./components/Accordion";
-import { useForm, FormProvider } from "react-hook-form";
-import FormInput from "./components/FormInput";
+import { useForm } from "react-hook-form";
 import ReactDOM from "react-dom/client";
 import Badge from "./components/Badge";
-import FormDatePicker from "./components/FormDatePicker";
 import { HiOutlineBell } from "react-icons/hi2";
 
 const App = () => {
-  const methods = useForm({
-    defaultValues: {
-      testInput: "",
-      emailInput: "",
-      telInput: "",
-      date1: "",
-      date2: "",
-    },
-  });
-
-  const {
-    handleSubmit,
-    formState: { errors },
-  } = methods;
-
-  const onSubmit = (data: any) => {
-    return;
-  };
-
   const accordionProps = {
     header: {
       title: (
@@ -72,67 +51,25 @@ const App = () => {
         </Accordion>
       </div>
 
-      <div className="flex flex-col gap-2">
-        <Badge type="success" content="Success" />
-        <Badge type="warning" content="Warning" />
-        <Badge type="alert" content="Alert" />
-      </div>
-      <div className="flex flex-col gap-4 p-4">
-        <FormProvider {...methods}>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-4 px-4"
-          >
-            <FormInput
-              label="Test Input"
-              name="testInput"
-              type="text"
-              register={methods.register}
-              errors={errors}
-              required={true}
-            />
-            <FormInput
-              label="Email Input"
-              name="emailInput"
-              type="email"
-              register={methods.register}
-              errors={errors}
-            />
-            <FormInput
-              label="Tel Input"
-              name="telInput"
-              type="tel"
-              register={methods.register}
-              errors={errors}
-              autocomplete={true}
-            />
-            <Button
-              title="Submit"
-              type="submit"
-              className="mt-4 p-2 bg-blue-500 text-white"
-            />
-          </form>
-        </FormProvider>
-      </div>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col">
-          <FormDatePicker
-            label="Label 1"
-            formField="date1"
-            onShowChange={() => {}}
-            control={methods.control}
-            setValue={methods.setValue}
-          />
+      <div className="flex flex-col min-w-72 gap-4">
+        <div className="flex flex-row">
+          <Badge template="success" content="Success" style="filled" />
         </div>
 
-        <div className="flex flex-col">
-          <FormDatePicker
-            label="Label 2"
-            formField="date2"
-            onShowChange={() => {}}
-            control={methods.control}
-            setValue={methods.setValue}
-          />
+        <div className="flex flex-row">
+          <Badge template="warning" content="Warning" />
+        </div>
+
+        <div className="flex flex-row">
+          <Badge template="alert" content="Alert" style="empty" compact />
+        </div>
+
+        <div className="flex flex-row">
+          <Badge template="default" content="Default" />
+        </div>
+
+        <div className="flex flex-row">
+          <Badge template="progress" content="Progress" style="empty" outline />
         </div>
       </div>
     </div>
