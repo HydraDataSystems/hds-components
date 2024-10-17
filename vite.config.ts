bd74006f7ebf4bfad997e297d50ad91d44106eb0
Francisco <hleyf@hotmail.com>
2024-10-17 15:37:22 +0100

    cssCodeSpilit add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts';
-import tsConfigPaths from 'vite-tsconfig-paths';
-import path, { resolve } from 'path';
-import * as packageJSON from './package.json';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
+import path, { resolve } from "path";
+import * as packageJSON from "./package.json";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths(), dts({ include:['src/**/*']})],
+  plugins: [react(), tsConfigPaths(), dts({ include: ["src/**/*"] })],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      "@": resolve(__dirname, "src"),
+    },
   },
   build: {
     lib: {
-      name: 'HdsComponents',
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
+      name: "HdsComponents",
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
       fileName: (ext) => `index.${ext}.js`,
-      // name: 'GlobalName' <-- for UMD 
+      // name: 'GlobalName' <-- for UMD
     },
+    cssCodeSplit: false,
     rollupOptions: {
-      external: [...Object.keys(packageJSON.peerDependencies), ...Object.keys(packageJSON.dependencies)]
+      external: [
+        ...Object.keys(packageJSON.peerDependencies),
+        ...Object.keys(packageJSON.dependencies),
+      ],
     },
     //target: 'esnext',
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
